6f969ddeeaca6d6bc78a92fb711ef22dff35f93c
Federico Galan <fegv.2@hotmail.com.ar>
2021-08-31 18:08:01 -0300

    the function that calculates the value of the cards is terminated

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,38 +3,40 @@ const types = ['C', 'D', 'H', 'S'];
 const aces = ['A', 'J', 'Q', 'K'];
 
 //This function create a new shuffle deck 
-
 const createDeck = () => {
-
     for ( let i = 2; i <= 10; i++ ) {
-
         for ( let type of types ) {
             deck.push( i + type );
         }
-
     }
-
     for ( let type of types ) {
         for ( let ace of aces ) {
             deck.push( ace + type );
         }
     }
-    deck = _.shuffle(deck);
-    console.log(deck);
+    deck = _.shuffle( deck );
 }
 
 createDeck();
 
-const pedirCarta = () => {
-
+//This function draw a new card
+const drawCard = () => {
     if (deck.length === 0) {
         throw 'No more cards in deck';
     }
-
-    const carta = deck.pop();
-    console.log(carta);
-    console.log(deck);
-    return carta;
+    const card = deck.pop();
+    console.log( card );
+    
+    return card;
 }
 
-pedirCarta();
+//this function returns the value of the card
+const cardValue = ( card ) => {
+    const value = card.substring( 0, card.length - 1 );
+    const points = (( isNaN( value ) ) ? ( value === 'A'  ?  11 : 10) : value * 1);   
+    
+    return points;
+}
+
+const value = cardValue(drawCard());
+console.log(value);
